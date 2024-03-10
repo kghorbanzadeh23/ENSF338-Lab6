2b8efd173611150b28a042056496e55ffadbd30e
HuttonLedingham <huttonledingham@gmail.com>
2024-03-10 14:33:31 -0600

    ex3 done

--- ex3.py
+++ ex3.py
@@ -7,75 +7,81 @@ class Node:
         self.right = None
 
 def is_operator(c):
-    return c in ['+', '-', '*', '/']
+    return c in ['+', '-', '*', '/', '–']
 
 def construct_tree(postfix):
     stack = []
     
     for char in postfix:
-        if not is_operator(char):
-            node = Node(char)
-            stack.append(node)
-        else:
-            node = Node(char)
-            right = stack.pop()
-            left = stack.pop()
-            node.right = right
-            node.left = left
-            stack.append(node)
+
+        if(char != '('):
+            if not is_operator(char):
+                node = Node(char)
+                stack.append(node)
+            else:
+                node = Node(char)
+
+                right = stack.pop()
+                left = stack.pop()
+                node.right = right
+                node.left = left
+                stack.append(node)
     
-    return stack.pop()
+    return stack[-1]
 
 def calculate(node):
-    if(node is not None):
-        if node.value.isdigit():
-            return int(node.value)
+    if node.value.isdigit():
+        return int(node.value)
         
-        left = calculate(node.left)
-        right = calculate(node.right)
+    left = calculate(node.left)
+    right = calculate(node.right)
         
-        print(left, right)
+    print(left, right)
 
-        if((left is not None) and (right is not None)):
-            if node.value == '+':
-                return left + right
-            elif node.value == '-':
-                return left - right
-            elif node.value == '*':
-                return left * right
-            elif node.value == '/':
-                return left // right
-    else:
-        return 0
+    if((left is not None) and (right is not None)):
+        if node.value == '+':
+            return left + right
+        elif node.value == '-' or node.value == '–':
+            return left - right
+        elif node.value == '*':
+            return left * right
+        elif node.value == '/':
+            return left // right
+
 
 def infix_to_postfix(expression):
     precedence = {'+':1, '-':1, '*':2, '/':2}
-    stack = [] 
-    postfix = [] 
-    for char in expression:
+    stack = []
+    postfix = []
+    i = 0
+    while i < len(expression):
+        char = expression[i]
         if char.isdigit():
-            postfix.append(char)
+            num = [char]
+            while i + 1 < len(expression) and expression[i+1].isdigit():
+                i += 1
+                num.append(expression[i])
+            postfix.append(''.join(num))
         elif char == '(':
             stack.append(char)
         elif char == ')':
             while stack and stack[-1] != '(':
                 postfix.append(stack.pop())
             if(len(stack) > 0):
-                stack.pop() 
+                stack.pop()
         else:
             while stack and precedence.get(char, 0) <= precedence.get(stack[-1], 0):
                 postfix.append(stack.pop())
             stack.append(char)
+        i += 1
+    
     while stack:
-        if(stack[-1] != '('):
-            postfix.append(stack.pop())
-        else:
-            stack.pop()
+        postfix.append(stack.pop())
     return postfix
-
     
 expression = sys.argv[1]
 expression = expression.replace(" ", "") 
+print(expression)
 postfix = infix_to_postfix(expression)
 print(postfix)
 tree = construct_tree(postfix)
